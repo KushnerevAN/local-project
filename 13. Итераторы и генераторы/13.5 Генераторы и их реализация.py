27a505d5f47276cf6b0c82cd7985a3cc79d356a1
KushnerevAN <Kushnerev@mail.ru>
2024-01-24 17:02:34 +0300

    24.01.2024

--- 13. Итераторы и генераторы/13.5 Генераторы и их реализация.py
+++ 13. Итераторы и генераторы/13.5 Генераторы и их реализация.py
@@ -1,25 +1,35 @@
 #Задача 1. Бесконечный генератор
 
-#def gen():
-#    count = 0
-#    while True:
-#        for i in range(count):
-#            yield i
-#        count += 1
-
-#res = gen()
-#for i in res:
-#    print(i)
-
-#Задача 2. Очень большой файл
 def gen():
-    file = open('number.txt', 'w', encoding='utf-8')
-
-    for i in file:
-        yield ' ','n'.join(i)
+    count = 0
+    while True:
+       yield count
+       count += 1
 
 res = gen()
-summ = 0
 for i in res:
-   summ += res
-   print(summ)
+    print(i)
+
+#Задача 2. Очень большой файл
+
+#def clear(line):
+#    return [int(number) for number in line.rstrip().split()]
+
+#def line(open_file):
+#    file = open(open_file, 'r', encoding='utf-8')
+#    for line in file:
+#        sum_clear_line = sum(clear(line))
+#        yield sum_clear_line
+
+#all_summ = 0
+#for num in line('number.txt'):
+#    all_summ += num
+#print(all_summ)
+
+
+
+
+
+
+
+

cadd63339597fa1fc94178ca791f1e26de652c7e
KushnerevAN <Kushnerev@mail.ru>
2024-03-22 17:31:21 +0300

    22.03.2024

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-class CustomButton:
-    def __init__(self, text, **kwargs):
-        self.text = text
 
-        def metod(self):
-            self.kwargs = kwargs
+class InfinityIterator:
 
-    def config(self, **kwargs):
-        self.__dict__.update(kwargs)
 
-    def click(self):
-        try:
-            self.command()
-        except AttributeError:
-            print('Кнопка не настроена')
-        except TypeError:
-            print('Кнопка сломалась')
 
-def func():
-    print('Оно живое')
 
-btn = CustomButton(text="Hello", bd=20, bg='#ffaaaa')
-btn.click()  # Кнопка не настроена
-btn.config(command=func)
-btn.click()  # Оно живое
+a = iter(InfinityIterator())
+next(a)
 
+next(a)
+
+next(a)
+
+next(a)
 

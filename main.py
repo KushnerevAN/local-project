b5efc733bec8e2f2f59cc72235cdc14f4f2aa43a
Admin <kushnerev@mail.ru>
2024-04-03 23:29:56 +0300

    03.04.2024

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
+from dataclasses import dataclass, field
 
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+@dataclass
+class Product:
+    name: str
+    price: float = field(repr=False)
 
-    def get_info(self):
-        return (f'Имя пользователя: {self.username}')
+@dataclass
+class Cart:
+    res: dict = field(default_factory=dict)
+    total: list = field(default_factory=list)
+    result: int = field(default_factory=int)
 
-class Authentication(User):
-    pass
+    def add_product(self, data):
+        self.res[data.name] = data.price
+        return self.res
 
-    def authenticate(self, new_name, new_password):
-        return new_name == self.username and new_password == self.password
+    def get_total(self):
+        for i in self.res.values():
+            self.total.append(i)
+        return sum(self.total)
 
-class AuthenticatedUser(Authentication, User):
-    pass
+    def apply_discount(self, value):
+        if 1 > value > 100:
+            raise ValueError('Неправильное значение скидки')
+        self.discount = sum(self.total) * value / 100
+        return sum(self.total) - self.discount
 
-assert issubclass(AuthenticatedUser, User) is True
-assert issubclass(AuthenticatedUser, Authentication) is True
 
-user1 = AuthenticatedUser('user1', 'password1')
-assert user1.get_info() == 'Имя пользователя: user1'
-assert user1.authenticate('user1', 'password2') is False
-assert user1.authenticate('user1', 'password1') is True
 
-ted = AuthenticatedUser('ted_lawyer', 'alligator3')
-print(ted.get_info())
+product1 = Product('Книга', 100.0)
+product2 = Product('Флешка', 50.0)
+product3 = Product('Ручка', 10.0)
+
+print(product1, product2, product3)
+
+cart = Cart()
+cart.add_product(product1)
+cart.add_product(product2)
+cart.add_product(product3)
+
+print(f"Стоимость товаров в корзине без скидки: {cart.get_total()}")
+# Применение скидки 10%
+cart.apply_discount(10)
+print(f"Стоимость товаров в корзине со скидкой 10%: {cart.get_total()}")

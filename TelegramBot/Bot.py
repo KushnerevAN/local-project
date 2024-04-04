e87e975dc5a06964278db46aece1cda52294a4cf
KushnerevAN <Kushnerev@mail.ru>
2024-04-04 18:22:41 +0300

    04.04.2024

--- TelegramBot/Bot.py
+++ TelegramBot/Bot.py
@@ -1,12 +1,12 @@
-import telebot
-from telebot import types
+#import telebot
+#from telebot import types
 
-bot = telebot.TeleBot('7169684743:AAFm_NcT1xF3ILx7KSFPagXLtAbjwqd_v9M')
+#bot = telebot.TeleBot('7169684743:AAFm_NcT1xF3ILx7KSFPagXLtAbjwqd_v9M')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
-    bot.send_message(message.chat.id, mess, parse_mode='html') # В () обращение к пользователю - сообщение - режим вывода
+#@bot.message_handler(commands=['start'])
+#def start(message):
+#    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+#    bot.send_message(message.chat.id, mess, parse_mode='html') # В () обращение к пользователю - сообщение - режим вывода
 
 #@bot.message_handler(content_types=['text'])
 #def get_user_text(message):
@@ -18,23 +18,23 @@ def start(message):
 #        photo = open('putin.jpeg', 'rb')
 #        bot.send_photo(message.chat.id, photo, )
 
-@bot.message_handler(content_types=['photo'])
-def get_user_photo(message):
-    bot.send_message(message.chat.id, 'Вау, какое крутое фото')
+#@bot.message_handler(content_types=['photo'])
+#def get_user_photo(message):
+#    bot.send_message(message.chat.id, 'Вау, какое крутое фото')
 
-@bot.message_handler(commands=['website']) # Создание
-def website(message):
-    markup = types.InlineKeyboardMarkup() # данный класс позволяет создавать встроенные в сообщения "вещи"
-    markup.add(types.InlineKeyboardButton("Посетить вэб сайт", url="https://ya.ru")) # создание кнопки перехода на сайт
-    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
+#@bot.message_handler(commands=['website']) # Создание
+#def website(message):
+#    markup = types.InlineKeyboardMarkup() # данный класс позволяет создавать встроенные в сообщения "вещи"
+#    markup.add(types.InlineKeyboardButton("Посетить вэб сайт", url="https://ya.ru")) # создание кнопки перехода на сайт
+#    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
 
-@bot.message_handler(commands=['help']) # Создание
-def website(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1) # данный класс позволяет создавать кнопки снизу (Подстройка под рзмер экрана, кол-во кнопок в ряду)
-    website = types.KeyboardButton('Вэб-сайт') # Создание кнопки
-    start = types.KeyboardButton('Старт')
-    markup.add(website, start)
+#@bot.message_handler(commands=['help']) # Создание
+#def website(message):
+#    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1) # данный класс позволяет создавать кнопки снизу (Подстройка под рзмер экрана, кол-во кнопок в ряду)
+#    website = types.KeyboardButton('Вэб-сайт') # Создание кнопки
+#    start = types.KeyboardButton('Старт')
+#    markup.add(website, start)
 
-    bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
+#bot.send_message(message.chat.id, 'Перейдите на сайт', reply_markup=markup)
 
-bot.polling(none_stop=True) #Запуск бота, который работает постоянно
+#bot.polling(none_stop=True) #Запуск бота, который работает постоянно
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from dataclasses import dataclass, field
 
+ACTIVE_PROMO = list
+
+@dataclass
+class Promo:
+    cod: str
+    discount: int
+    spisok_tovarov: list = field(default=False)
+
+    def __post_init__(self):
+        if self.discount > 100 or self.discount < 1:
+            self.discount = 0
+
 @dataclass
 class Product:
     name: str
@@ -7,39 +19,58 @@ class Product:
 
 @dataclass
 class Cart:
-    res: dict = field(default_factory=dict)
-    total: list = field(default_factory=list)
-    result: int = field(default_factory=int)
+    bag: list = field(default_factory=list, repr=False)
+    total_bag:  float = field(default=0)
+    res_disc: int = field(default=0)
 
-    def add_product(self, data):
-        self.res[data.name] = data.price
-        return self.res
+    def add_product(self, data, num=1):
+        self.bag.append([data.name, data.price, num])
+
+
 
     def get_total(self):
-        for i in self.res.values():
-            self.total.append(i)
-        return sum(self.total)
+        self.total_bag = sum(i[1] * i[2] for i in self.bag)
+        if self.apply_promo:
+            return self.total_bag - self.res_disc
+        return self.total_bag
+
+    def apply_promo(self, promo):
+        count = 0
+        print(self.bag)
+        for i in ACTIVE_PROMO:
+            if promo == i.cod and not i.spisok_tovarov:
+                count += 1
+                self.res_disc = self.total_bag * i.discount / 100
+                print(f'Промокод {promo} успешно применился')
+                break
+            if promo == i.cod and i.spisok_tovarov:
+                count += 1
+                self.res_disc = sum(i[0] * i[1] for i in self.bag[]) * i.discount / 100
+                print(f'Промокод {promo} успешно применился')
+                break
+        if count == 0:
+            print(f'Промокода {promo} не существует')
 
     def apply_discount(self, value):
-        if 1 > value > 100:
+        if value > 100 or value < 1:
             raise ValueError('Неправильное значение скидки')
-        self.discount = sum(self.total) * value / 100
+        self.res_disc = self.total_bag * value / 100
 
 
+book = Product('Книга', 100.0)
+usb = Product('Флешка', 50.0)
+pen = Product('Ручка', 10.0)
 
-
-product1 = Product('Книга', 100.0)
-product2 = Product('Флешка', 50.0)
-product3 = Product('Ручка', 10.0)
-
-print(product1, product2, product3)
+ACTIVE_PROMO = [
+    Promo('new', 20, [pen]),
+    Promo('all_goods', 30),
+]
 
 cart = Cart()
-cart.add_product(product1)
-cart.add_product(product2)
-cart.add_product(product3)
+cart.add_product(book, 10)
+cart.add_product(pen)
+print(cart.get_total())
 
-print(f"Стоимость товаров в корзине без скидки: {cart.get_total()}")
-# Применение скидки 10%
-cart.apply_discount(10)
-print(f"Стоимость товаров в корзине со скидкой 10%: {cart.get_total()}")
+# Применение промокода в 20% на ручку
+cart.apply_promo('new')
+print(cart.get_total())

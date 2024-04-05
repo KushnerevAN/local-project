5a464725f25943e6a1894b6920c343179b33c0d6
KushnerevAN <Kushnerev@mail.ru>
2024-04-05 17:17:09 +0300

    05.04.2024

--- main.py
+++ main.py
@@ -17,44 +17,56 @@ class Product:
     name: str
     price: float = field(repr=False)
 
+
 @dataclass
 class Cart:
-    bag: list = field(default_factory=list, repr=False)
+    bag: dict = field(default_factory=dict)
     total_bag:  float = field(default=0)
-    res_disc: int = field(default=0)
+    res_disc: float = field(default=0)
+    quantity: dict = field(default_factory=dict)
 
     def add_product(self, data, num=1):
-        self.bag.append([data.name, data.price, num])
-
-
+        self.bag[data.name] = data.price
+        if data.name in self.quantity.keys():
+            self.quantity[data.name] += num
+        else:
+            self.quantity[data.name] = num
+        print(self.bag, self.quantity)
 
     def get_total(self):
-        self.total_bag = sum(i[1] * i[2] for i in self.bag)
+        for i in self.bag.keys():
+            self.total_bag += (self.bag[i] * self.quantity[i])
+        print(self.total_bag, 'gbpltw')
         if self.apply_promo:
+            #print(self.res_disc)
             return self.total_bag - self.res_disc
         return self.total_bag
 
     def apply_promo(self, promo):
         count = 0
-        print(self.bag)
         for i in ACTIVE_PROMO:
             if promo == i.cod and not i.spisok_tovarov:
+                #print(promo, i)
                 count += 1
                 self.res_disc = self.total_bag * i.discount / 100
                 print(f'Промокод {promo} успешно применился')
-                break
             if promo == i.cod and i.spisok_tovarov:
-                count += 1
-                self.res_disc = sum(i[0] * i[1] for i in self.bag[]) * i.discount / 100
-                print(f'Промокод {promo} успешно применился')
-                break
+                for k in i.spisok_tovarov:
+                   # print(self.bag[k.name])
+                   # print(self.quantity[k.name])
+                    #print(i.discount)
+                    count += 1
+                    self.res_disc = float(self.bag[k.name] * self.quantity[k.name]) * i.discount / 100
+                    print(self.res_disc, 'fgfgdgfg')
+                    print(f'Промокод {promo} успешно применился')
         if count == 0:
             print(f'Промокода {promo} не существует')
 
-    def apply_discount(self, value):
-        if value > 100 or value < 1:
-            raise ValueError('Неправильное значение скидки')
-        self.res_disc = self.total_bag * value / 100
+
+    #def apply_discount(self, value):
+    #    if value > 100 or value < 1:
+    #        raise ValueError('Неправильное значение скидки')
+    #    #self.res_disc = self.total_bag * value / 100
 
 
 book = Product('Книга', 100.0)
@@ -64,6 +76,7 @@ pen = Product('Ручка', 10.0)
 ACTIVE_PROMO = [
     Promo('new', 20, [pen]),
     Promo('all_goods', 30),
+    Promo('only_book', 40, [book]),
 ]
 
 cart = Cart()
@@ -71,6 +84,6 @@ cart.add_product(book, 10)
 cart.add_product(pen)
 print(cart.get_total())
 
-# Применение промокода в 20% на ручку
-cart.apply_promo('new')
+# Применение промокода в 40% на книгу
+cart.apply_promo('only_book')
 print(cart.get_total())
